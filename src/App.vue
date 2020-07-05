9312e3363ec74701c1a9bc4aaf47144a52d1e7dd
Kutay Çelebi <celebikutay@gmail.com>
2020-07-05 18:46:29 +0300

    Slot example

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello World"/>
-    <Counter/>
-    <TwoWayBind/>
-    <ConditionalRender/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Hello World"/>
+        <Counter/>
+        <TwoWayBind/>
+        <ConditionalRender/>
+        <SlotComponent>
+            <p>slot component content</p>
+        </SlotComponent>
+    </div>
 </template>
 
 <script>
-import HelloWorld        from './components/HelloWorld.vue'
-import Counter           from "@/components/Counter";
-import TwoWayBind        from "@/components/TwoWayBind";
-import ConditionalRender from "@/components/ConditionalRender";
+    import HelloWorld        from './components/HelloWorld.vue'
+    import Counter           from "@/components/Counter";
+    import TwoWayBind        from "@/components/TwoWayBind";
+    import ConditionalRender from "@/components/ConditionalRender";
+    import SlotComponent     from "./components/SlotComponent";
 
-export default {
-  name: 'App',
-  components: {
-    ConditionalRender,
-    TwoWayBind,
-    HelloWorld,
-    Counter,
-  }
-}
+    export default {
+        name      : 'App',
+        components: {
+            SlotComponent,
+            ConditionalRender,
+            TwoWayBind,
+            HelloWorld,
+            Counter,
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/SlotComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bordereddiv">
+        <h2>Slot ex.</h2>
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SlotComponent"
+    }
+</script>
+
+<style scoped>
+    .bordereddiv {
+        padding: 1.25rem;
+        margin: 1.25rem;
+        border: 1px solid black;
+    }
+
+</style>
